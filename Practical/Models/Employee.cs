1cd7fd7276f7d490d2231cb63799ad987bbb73d3
LinhVuHoang <56089011+LinhVuHoang@users.noreply.github.com>
2021-06-14 20:35:11 +0700

    Add project files.

---
+++ Practical/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical.Models
+{
+    public class Employee
+    {
+       
+        public string name { get; set; }
+        public string role { get; set; }
+        public string birthyear { get; set; }
+    }
+
+    public class Root
+    {
+        public List<Employee> employee_list { get; set; }
+    }
+
+
+}
